3f8c739a75bdc5b8f6e954736df5c0003cfd74f7
zhaoguangtao <2560833848@qq.com>
2025-01-07 18:19:47 +0800

    chore: 全局module修改

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { UserModule } from './user/user.module';
+import { AppModule } from './app.module';
 import { TransformInterceptor } from './app.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(UserModule);
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('v1');
   app.useGlobalInterceptors(new TransformInterceptor());
   
